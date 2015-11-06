c16cba6ca2a4b4d41fc275c8712dc3c930100dbb
banyudu <banyudu@gmail.com>
2015-11-06 16:19:56 +0800

    optimize c/c++ style

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -1,5 +1,16 @@
 ;; init-cc-mode.el
 
-(setq c-basic-offset 4)
+;; styles
+(setq-default c-basic-offset 4
+	      tab-width 4
+	      indent-tabs-mode t)
+(defun my-c-mode-hook ()
+  (c-set-style "bsd")
+  (setq shift-width 4)
+  (setq c-basic-offset 4))
+(add-hook 'c-mode-common-hook 'my-c-mode-hook)
+
+;; .h file as c++-mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 (provide 'init-cc-mode)
