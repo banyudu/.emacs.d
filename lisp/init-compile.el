1738b0d373b523b73c785d1ab13dd834371917ec
banyudu <banyudu@gmail.com>
2016-01-08 16:22:51 +0800

    enhance compile mode configurations

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -1,3 +1,10 @@
 ;; init-compile.el
 (setq compilation-scroll-output t)
+(add-hook 'compilation-mode-hook
+          '(lambda()
+;;             (setq compilation-auto-jump-to-first-error t)
+             (setq compilation-scroll-output t
+                   compilation-window-height 10)
+             (define-key compilation-mode-map (kbd "n") 'compilation-next-error)
+             (define-key compilation-mode-map (kbd "p") 'compilation-previous-error)))
 (provide 'init-compile)
