afc747d28171af5c711a8015e3690a06fc20deb3
banyudu <banyudu@gmail.com>
2016-01-28 17:35:03 +0800

    show speedbar on start up

--- lisp/init-speedbar.el
+++ lisp/init-speedbar.el
@@ -7,4 +7,9 @@
 (require 'projectile-speedbar)
 (global-set-key (kbd "M-<f2>") 'projectile-speedbar-open-current-buffer-in-tree)
 (setq sr-speedbar-right-side nil)
+
+;; open speedbar on start up
+(add-hook 'emacs-startup-hook (lambda ()
+  (sr-speedbar-open)
+  ))
 (provide 'init-speedbar)
