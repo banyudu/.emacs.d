0912eddc9d4294f7eae5c326d55dd651e6b8e2d9
banyudu <banyudu@gmail.com>
2016-01-28 16:35:17 +0800

    add remark info

--- lisp/init-tabbar.el
+++ lisp/init-tabbar.el
@@ -3,18 +3,19 @@
 (require-package 'tabbar)
 (tabbar-mode 1)
 
+;; tabbar faces modified from http://blog.csdn.net/CherylNatsu/article/details/6204737
 (set-face-attribute 'tabbar-default nil  
                     :family "Monaco"  
                     :background "gray80"  
                     :foreground "gray30"  
                     :height 1.0  
                     )  
-;; 设置左边按钮外观：外框框边大小和颜色  
+;; buttons
 (set-face-attribute 'tabbar-button nil  
                     :inherit 'tabbar-default  
                     :box '(:line-width 1 :color "yellow70")  
                     )  
-;; 设置当前tab外观：颜色，字体，外框大小和颜色  
+;; selected tab
 (set-face-attribute 'tabbar-selected nil  
                     :inherit 'tabbar-default  
                     :foreground "DarkGreen"  
@@ -24,10 +25,11 @@
                     :underline "black"  
                     :weight 'bold  
                     )  
-;; 设置非当前tab外观：外框大小和颜色  
+;; unselected tabs
 (set-face-attribute 'tabbar-unselected nil  
                     :inherit 'tabbar-default
 					:foreground "DeepSkyBlue4"
                     :box '(:line-width 2 :color "LightGoldenrod")
-                    )  
+                    )
+
 (provide 'init-tabbar)
