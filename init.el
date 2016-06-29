2daca45815cbf0861efd83a17ab271ddf9463e6c
banyudu <banyudu@gmail.com>
2016-06-29 12:36:09 +0800

    require header2 package on start up

--- init.el
+++ init.el
@@ -88,3 +88,20 @@
 
 ;; css
 (require 'init-css)
+
+;; header
+(require 'init-header)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+	(header2 youdao-dictionary yaml-mode tabbar-ruler sr-speedbar session scss-mode pkg-info nlinum markdown-mode less-css-mode json-snatcher json-reformat google-translate flx-ido ecb dash auto-complete-nxml auto-complete-c-headers))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-header.el
@@ -0,0 +1,6 @@
+;; init-header.el
+
+;; require package header2
+(require-package 'header2)
+
+(provide 'init-header)
