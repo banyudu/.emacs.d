392ebbe0a9ea37508763c182608bb532c2b6ea3b
banyudu <banyudu@gmail.com>
2017-01-08 20:20:42 +0800

    modify indents of js related files

--- lisp/init-coffee.el
+++ lisp/init-coffee.el
@@ -2,4 +2,7 @@
 
 (require-package 'coffee-mode)
 
+;; This gives you a tab of 2 spaces
+(custom-set-variables '(coffee-tab-width 2))
+
 (provide 'init-coffee)
--- lisp/init-json.el
+++ lisp/init-json.el
@@ -2,4 +2,9 @@
 ;;(require-package 'json-reformat)
 (require-package 'json-mode)
 
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
+
 (provide 'init-json)
